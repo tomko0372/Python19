f4c976355b7eec2495acb2245bee99c3508dbabd
KowalTo3 <tomasz.kowalczyk2@orange.com>
2022-12-20 13:04:12 +0100

    wt  12:45

--- funkcje2.py
+++ funkcje2.py
@@ -15,3 +15,6 @@ def druga():
 
 print(pierwsza())
 print(druga())
+print(a)
+
+
---
+++ funkcje3.py
@@ -0,0 +1,16 @@
+def funkcja(x):
+    return x * 2
+
+
+fun = lambda x: x * 2
+
+print(f"wynik działania funkcji {funkcja(5)}")
+print(f"wynik działania funkcji {funkcja(15)}")
+print(f"wynik działania funkcji {funkcja(25)}")
+print(f"wynik działania funkcji {fun(30)}")
+print(f"wynik działania funkcji {(lambda x:x*2)(35)}")
+
+wiek = lambda x: "Dziecko" if x < 10 else ("Nastolatek" if x < 18 else "Dorosły")
+print(wiek(5))
+print(wiek(15))
+print(wiek(25))
